6309b1230de551a9fe9fe4c983bbea992cc835d3
Johannes Mols <johannes.mols@googlemail.com>
2021-11-28 14:17:03 +0100

    Space

--- src/miner.py
+++ src/miner.py
@@ -68,7 +68,7 @@ class Miner:
         if self.do_conformance_check:
             os.makedirs('../xes-files', exist_ok=True)
             os.makedirs('../conf-check', exist_ok=True)
-            self.xes = xes_importer.apply(f'../xes-files/{self.log_name}.xes', variant=xes_importer.Variants.ITERPARSE,parameters={xes_importer.Variants.ITERPARSE.value.Parameters.TIMESTAMP_SORT: True})
+            self.xes = xes_importer.apply(f'../xes-files/{self.log_name}.xes', variant=xes_importer.Variants.ITERPARSE, parameters={xes_importer.Variants.ITERPARSE.value.Parameters.TIMESTAMP_SORT: True})
             self.xes_conf_file = open(f'../conf-check/{self.log_name}.csv', 'w')
             self.xes_conf_file.write('Events,Fitness\n')
 
